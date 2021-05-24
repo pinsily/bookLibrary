8bc3fcbbfecc6429ff43d884903b612d0a1a3f32
pinsily <13160724868@163.com>
2021-05-24 23:41:12 +0800

    add:requirements

--- bookLibrary/settings.py
+++ bookLibrary/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'booklibrary',
         'USER': 'root',
-        'PASSWORD': 'pinsily96',
+        'PASSWORD': 'Root20..',
         'HOST': 'localhost',
         'PORT': '3306',
     }
